38c942f93a5cadc2228c70723f37ee9bf8753a7f
Muhsin Gnuaydin <muhsing@viken.no>
2021-08-24 13:16:27 +0200

    lagt til at man kan regne ut areal og omkrets av firekat

---
+++ Uke 1/MatteMedPython.py
@@ -0,0 +1,11 @@
+
+
+#Regne ut areal av firekant:
+hoyde = int(input("Skriv høyden: "))
+bredde = int(input("Skriv bredden:"))
+
+areal = bredde*hoyde
+omkrets = 2*bredde + 2*hoyde
+
+print("Arealet til objektet er: " + str(areal))
+print("Omkretsen er: " + str(omkrets))
