fea6bf306e6a8da6d7cea43a9e067227943eeef2
Muhsin Gnuaydin <muhsing@viken.no>
2021-08-24 13:27:01 +0200

    lagt til areal av trekant

--- Uke 1/MatteMedPython.py
+++ Uke 1/MatteMedPython.py
@@ -9,3 +9,12 @@ omkrets = 2*bredde + 2*hoyde
 
 print("Arealet til objektet er: " + str(areal))
 print("Omkretsen er: " + str(omkrets))
+
+
+#Regne ut areal av en trekant
+grunnlinje = int(input("Hva er grunnlinja: "))
+hoyde = int(input("Hva er høyden: "))
+areal = grunnlinje*hoyde/2
+print("Arealet er : " + str(areal))
+
+
